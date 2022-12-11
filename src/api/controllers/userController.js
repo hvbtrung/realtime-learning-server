3e17527d5837ff385f1f6296ff178852a56a97dc
vinhnguyen2001 <vinhnguyentran2001@gmail.com>
2022-12-11 13:39:44 +0700

    remove redundant code

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,117 +1,119 @@
-const User = require('../models/userModel');
+const User = require("../models/userModel");
 
 const filterObj = (obj, ...allowedFields) => {
-    const newObj = {};
-    Object.keys(obj).forEach(el => {
-        if (allowedFields.includes(el)) newObj[el] = obj[el];
-    });
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
 
-    return newObj;
-}
+  return newObj;
+};
 
 exports.getMe = async (req, res, next) => {
-    req.params.id = req.user._id;
-    next();
-}
+  req.params.id = req.user._id;
+  next();
+};
 
 exports.updateMe = async (req, res, next) => {
-    if (req.body.password) {
-        const err = new Error('This route is not for password updates. Please use /updateMyPassword');
-        err.statusCode = 400;
-        throw err;
-    }
+  if (req.body.password) {
+    const err = new Error(
+      "This route is not for password updates. Please use /updateMyPassword"
+    );
+    err.statusCode = 400;
+    throw err;
+  }
 
-    const filteredBody = filterObj(req.body, 'name', 'photo');
+  const filteredBody = filterObj(req.body, "name", "photo");
 
-    const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
-        new: true,
-        runValidators: true
-    });
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            user: updatedUser
-        }
-    })
-}
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
+};
 
 exports.deleteMe = async (req, res, next) => {
-    await User.findByIdAndUpdate(req.user._id, { active: false });
+  await User.findByIdAndUpdate(req.user._id, { active: false });
 
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
-}
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+};
 
 exports.getUsers = async (req, res, next) => {
-    const users = await User.find();
+  const users = await User.find();
 
-    if (!users.length) {
-        const err = new Error('No users found');
-        err.statusCode = 400;
-        throw err;
-    }
+  if (!users.length) {
+    const err = new Error("No users found");
+    err.statusCode = 400;
+    throw err;
+  }
 
-    res.status(200).json({
-        status: 'success',
-        results: users.length,
-        data: { users }
-    });
-}
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: { users },
+  });
+};
 
 exports.getUser = async (req, res, next) => {
-    const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id);
 
-    if (!user) {
-        const err = new Error('No user found with that ID');
-        err.statusCode = 404;
-        throw err;
-    }
+  if (!user) {
+    const err = new Error("No user found with that ID");
+    err.statusCode = 404;
+    throw err;
+  }
 
-    res.status(200).json({
-        status: 'success',
-        data: { user }
-    });
-}
+  res.status(200).json({
+    status: "success",
+    data: { user },
+  });
+};
 
 exports.createUser = (req, res, next) => {
-    const err = new Error('This route is not defined! Please use /signup');
-    err.statusCode = 500;
-    throw err;
-}
+  const err = new Error("This route is not defined! Please use /signup");
+  err.statusCode = 500;
+  throw err;
+};
 
 // Do NOT update password
 exports.updaterUser = async (req, res, next) => {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    });
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!user) {
-        const err = new Error('No user found with that ID');
-        err.statusCode = 404;
-        throw err;
-    }
+  if (!user) {
+    const err = new Error("No user found with that ID");
+    err.statusCode = 404;
+    throw err;
+  }
 
-    res.status(200).json({
-        status: 'success',
-        data: { user }
-    });
-}
+  res.status(200).json({
+    status: "success",
+    data: { user },
+  });
+};
 
 exports.deleteUser = async (req, res, next) => {
-    const user = await User.findByIdAndDelete(req.params.id);
+  const user = await User.findByIdAndDelete(req.params.id);
 
-    if (!user) {
-        const err = new Error('No user found with that ID');
-        err.statusCode = 404;
-        throw err;
-    }
+  if (!user) {
+    const err = new Error("No user found with that ID");
+    err.statusCode = 404;
+    throw err;
+  }
 
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
-}
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+};
--- src/api/models/groupDetailModel.js
+++ src/api/models/groupDetailModel.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const { ObjectId } = mongoose.Schema;
 
 const groupDetailSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: "User" },
