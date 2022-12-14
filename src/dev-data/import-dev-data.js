ea395aff48b4eca670f3fed3fb7f2701ecb6697b
hvbtrung <hvbtrung@gmail.com>
2022-12-14 20:45:30 +0700

    Update dev-data

--- src/dev-data/import-dev-data.js
+++ src/dev-data/import-dev-data.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const User = require('../api/models/userModel');
 const Group = require('../api/models/groupModel');
 const GroupDetail = require('../api/models/groupDetailModel');
+const Presentation = require('../api/models/presentationModel');
+const Slide = require('../api/models/slideModel');
 
 mongoose
     .connect(process.env.MONGO_URI)
@@ -15,12 +17,16 @@ mongoose
 const users = JSON.parse(fs.readFileSync(`${__dirname}/user.json`, 'utf-8'));
 const groups = JSON.parse(fs.readFileSync(`${__dirname}/groups.json`, 'utf-8'));
 const groupDeltails = JSON.parse(fs.readFileSync(`${__dirname}/groupdetails.json`, 'utf-8'));
+const presentations = JSON.parse(fs.readFileSync(`${__dirname}/presentations.json`, 'utf-8'));
+const slides = JSON.parse(fs.readFileSync(`${__dirname}/slides.json`, 'utf-8'));
 
 const importData = async () => {
     try {
         await User.create(users, { validateBeforeSave: false });
         await Group.create(groups, { validateBeforeSave: false });
         await GroupDetail.create(groupDeltails, { validateBeforeSave: false });
+        await Presentation.create(presentations, { validateBeforeSave: false });
+        await Slide.create(slides, { validateBeforeSave: false });
         console.log('Data successfully loaded!');
     } catch (err) {
         console.log(err);
